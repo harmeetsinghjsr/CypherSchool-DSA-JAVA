5b4aab08ed689eb36afd60d7e5578bf0d9102bb3
Harmeet <hs978698@gmail.com>
2024-06-05 12:28:19 +0530

    first.java

--- first.java
+++ first.java
@@ -1,5 +1,3 @@
-package CypherSchool-Java And Data Structures & Algorithm Program  Hybrid;
-
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello World");
