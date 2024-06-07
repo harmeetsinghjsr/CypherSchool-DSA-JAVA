dce7930aea57ecb559e383537f502474cbd033f2
Harmeet <hs978698@gmail.com>
2024-06-07 14:05:03 +0530

    primitive datatypes part 2

---
+++ PrimitiveDT.java
@@ -0,0 +1,34 @@
+public class PrimitiveDT {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        // 1. Integer
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Sum of a and b is: " + c);
+
+        // 2. Float
+        float d = 10.5f;
+        float e = 20.5f;
+        float f = d + e;
+        System.out.println("Sum of d and e is: " + f);
+
+        // 3. Double
+        double g = 10.5;
+        double h = 20.5;
+        double i = g + h;
+        System.out.println("Sum of g and h is: " + i);
+
+        // 4. Character
+        char j = 'A';
+        char k = 'B';
+        System.out.println("Character j is: " + j);
+        System.out.println("Character k is: " + k);
+
+        // 5. Boolean
+        boolean l = true;
+        boolean m = false;
+        System.out.println("Boolean l is: " + l);
+        System.out.println("Boolean m is: " + m);
+    }
+}
