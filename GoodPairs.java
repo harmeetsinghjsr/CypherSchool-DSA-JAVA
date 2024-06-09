9a4d5ea0b13fcc864818b0f7c3e9029e44184106
Harmeet <hs978698@gmail.com>
2024-06-09 09:51:25 +0530

    leetcode question 1512

---
+++ GoodPairs.java
@@ -0,0 +1,20 @@
+//Leetcode question: https://leetcode.com/problems/number-of-good-pairs/
+public class GoodPairs {
+    public static int countGoodPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        int result = countGoodPairs(nums);
+        System.out.println("Number of good pairs: " + result);
+    }
+}
