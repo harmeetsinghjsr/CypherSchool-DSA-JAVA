31210737eb77b1539065f937cb6befe917c85a55
Harmeet <hs978698@gmail.com>
2024-06-09 10:45:31 +0530

    leet code question 1470

---
+++ ShuffledPair.java
@@ -0,0 +1,25 @@
+//Leetcode: https://leetcode.com/problems/shuffle-the-array/
+public class ShuffledPair {
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            result[index++] = nums[i];
+            result[index++] = nums[i + n];
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 1, 3, 4, 7};
+        int n = 3;
+        
+        int[] shuffledArray = shuffle(nums, n);
+        
+        for (int num : shuffledArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
