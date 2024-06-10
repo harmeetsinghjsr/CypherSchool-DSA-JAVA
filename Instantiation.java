f734bfa1f7a8d67fc37175450bff7122fb40c983
Harmeet <hs978698@gmail.com>
2024-06-10 11:16:26 +0530

    objects referencing

---
+++ Instantiation.java
@@ -0,0 +1,9 @@
+public class Instantiation {
+    public void print() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        Instantiation obj = new Instantiation();
+        obj.print();
+    }
+}
