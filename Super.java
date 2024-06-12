bd23dfb613156c73c080ab2f6577e01894673903
Harmeet <hs978698@gmail.com>
2024-06-12 17:45:45 +0530

    constructor

---
+++ Super.java
@@ -0,0 +1,41 @@
+class Loan
+{
+    int rate;
+    Loan()
+    {
+        System.out.println("default constructor");
+    }
+    Loan(int rate)
+    {
+        this.rate = rate;
+    }
+    void show()
+    {
+        System.out.println("Rate of interest is: "+this.rate);
+    }
+}
+class HomeLoan extends Loan
+{
+    int rate;
+    HomeLoan()
+    {
+        System.out.println("default constructor");
+        System.out.println(super.rate);
+    }
+    HomeLoan(int rate)
+    {
+        super(7);
+        this.rate = rate;
+    }
+    void show()
+    {
+        System.out.println("Rate of interest is: "+this.rate);
+        super.show();
+        System.out.println("Rate of interest is: "+super.rate);
+    }
+public class Super {
+    public static void main(String[] args) {
+        HomeLoan obj = new HomeLoan(8);
+        obj.show();
+    }
+}
