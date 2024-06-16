b7e2851acd668b6bb9fc9162355f523216842438
Harmeet <hs978698@gmail.com>
2024-06-16 17:56:32 +0530

    array

--- array.java
+++ array.java
@@ -1,12 +1,15 @@
 public class array {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4};
-        int arr1[0]=1;
-        int arr1[1]=arr[0];
+        int arr1[] = new int[2];
+        arr1[0] = 1;
+        arr1[1] = arr[0];
         System.out.println(arr[0]);
         System.out.println(arr[1]);
         System.out.println(arr[2]);
         System.out.println(arr[3]);
-        System.out.println(arr1);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
     }
 }
