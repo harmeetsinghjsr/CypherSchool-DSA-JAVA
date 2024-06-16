a3fcab54089c34668bbb614ea5340b03f6cfe7e3
Harmeet <hs978698@gmail.com>
2024-06-16 17:55:38 +0530

    array learning

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4};
+        int arr1[0]=1;
+        int arr1[1]=arr[0];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr1);
+    }
+}
