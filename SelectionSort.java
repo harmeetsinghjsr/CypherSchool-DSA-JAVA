b6ad47043a04add73b90b73bcf851da0aad17795
Harmeet <hs978698@gmail.com>
2024-06-26 19:40:04 +0530

    java code for selection sort algorithm

---
+++ SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: "+n);
+        int n=in.nextInt();
+        System.out.println("Enter the elements of the array: ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        System.out.println("Sorted Array is: ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
