1bb7e2b1a65170e0d596f9918724bcfda5ef6a8c
Harmeet <hs978698@gmail.com>
2024-06-26 14:33:53 +0530

    time complexity

---
+++ TimeComplexity.java
@@ -0,0 +1,41 @@
+public class TimeComplexity {
+    static boolean isPrimeRam(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n==2 || n==3) {
+            return true;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    static boolean isPrimeShyam(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n==2 || n==3) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 100;
+        long start = System.currentTimeMillis();
+        System.out.println(isPrimeRam(n));
+        long end = System.currentTimeMillis();
+        System.out.println("Time by RAM's approach: "+(end-start));
+        start = System.currentTimeMillis();
+        System.out.println(isPrimeSham(n));
+        end = System.currentTimeMillis();
+        System.out.println("Time by SHAM's approach: "+(end-start));
+    }
+}
