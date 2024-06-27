f0a969eea870b86b9071412b00a0f542cfe199ab
Harmeet <hs978698@gmail.com>
2024-06-27 11:30:23 +0530

    strings part 2 lecture 23

---
+++ String2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class String2{
+public static void main(String[] args)
+{
+String name = "Cipher Schools";
+String name2 = "Cipher Schools";
+String name3 = new String("Cipher Schools");
+String name4 = new String("Cipher Schools");
+
+System.out.println(name==name2);
+System.out.println(name3==name4);
+System.out.println(name==name3);
+
+String s1 = "Hello";
+s1= s1 +"peeps";
+System.out.println(s1);
+System.out.println(s1+",how're you doing");
+System.out.println(s1);
+
+String s2= new String("Hello");
+String s3= new String("People");
+String s4= s2.concat(s3);
+System.out.println(s4);
+System.out.println(s2);
+
+System.out.println("*******************");
+
+System.out.println("CHECKING STRINGS ARE EQUAL IN VALUE");
+
+System.out.println(name.equals(name2));
+System.out.println(name.equals(name3));
+
+System.out.println("*******************");
+
+System.out.println("3) Creating new string object from character array");
+
+char arr[] = {'C', 'I', 'P', 'H', 'E', 'R'};
+String strfromArr = new String(arr);
+System.out.println(strfromArr);
+
+
+System.out.println("*******************");
+
+System.out.println("4) Creating partial string object from character array");
+String pstrfromArr = new String(arr,1,3);
+System.out.println(pstrfromArr);
+String pstrfromArr2 = new String(arr,2,3);
+System.out.println(pstrfromArr2);
+
+System.out.println("*******************");
+
+System.out.println("5) Changing cases");
+System.out.println(name.toLowerCase());
+System.out.println(name.toUpperCase());
+System.out.println(name);
+
+}
+}
