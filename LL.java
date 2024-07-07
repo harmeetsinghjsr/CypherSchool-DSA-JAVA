f5b69a3f829d87a878af31ffb09ce005856e2a50
Harmeet <hs978698@gmail.com>
2024-07-07 15:21:41 +0530

    completing linkedlist

---
+++ LL.java
@@ -0,0 +1,12 @@
+public class LL {
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+}
